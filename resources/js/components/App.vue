9cde41ef5b2528c5610a2070bac9171d2352c624
pjotrvp <74866171+pjotrvp@users.noreply.github.com>
2024-12-17 12:44:29 +0100

    Add models, migrations, and Vue components for transaction tracking

---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Prui fin!</h1>
+        <p>Track what you earn and what you spend!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
